79c44fd69d0d2b54ab9cb7f7fc37d1513106fad8
quangthucne <thucbqpc08717@gmail.com>
2024-10-09 09:36:09 +0700

    update index
    add doGet
    replace path img
    replace icon theme icon -> fontawesome

--- WebsiteSellPhone/src/main/java/com/poly/websitesellphone/controller/IndexController.java
+++ WebsiteSellPhone/src/main/java/com/poly/websitesellphone/controller/IndexController.java
@@ -12,11 +12,12 @@ import jakarta.servlet.http.HttpServletResponse;
 public class IndexController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		req.getRequestDispatcher("/views/user/index.jsp").forward(req, resp);
+
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/webapp/views/user/index.jsp").forward(req, resp);
+		req.getRequestDispatcher("/views/user/index.jsp").forward(req, resp);
 	}
 
 }
